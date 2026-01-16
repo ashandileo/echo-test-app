93b3728c159e372de427ddedd91f6edc51761ac4
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2026-01-16 22:20:39 +0700

    fix: callback url

--- app/auth/v1/callback/route.ts
+++ app/auth/v1/callback/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: Request) {
+  console.log("Testing");
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   const origin = requestUrl.origin;
@@ -10,6 +11,8 @@ export async function GET(request: Request) {
   if (code) {
     const supabase = await createClient();
     const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+
+    console.log("data", data);
 
     if (!error && data.user) {
       // Check if user has a profile
